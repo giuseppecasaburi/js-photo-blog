d98b63ed63fbabad0db95e20813cd706e8cb6256
giuseppecasaburi <enzobiondi0000@libero.it>
2024-11-20 17:58:10 +0100

    feat: refactoring codice

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
+// ELEMENTI DOM
 const rigaElm = document.querySelector(".riga");
 const overElm = document.getElementById("overlay");
 const closeElm = document.getElementById("close");
 const contOverlayElm = document.getElementById("cont-overlay");
 
+// FUNCTION
+/**
+ * RIMUOVE L'OVERLAY DAL VIEWPORT
+ * @returns {any}
+ */
 function remove () {
     overElm.classList.remove("flex");
     overElm.classList.add("hidden");
     contOverlayElm.innerHTML = "";
 };
 
+/**
+ * PRENDE ARRAY DI CARD E AGGIUNGE EVENTO CLICK CON OVERLAY 
+ * @returns {any}
+ */
+function overlay () {
+    const cardElm = document.querySelectorAll(".card");
+        cardElm.forEach((card, index) => {
+            card.addEventListener("click", function () {
+                overElm.classList.remove("hidden");
+                overElm.classList.add("flex");
+                const url = card.dataset.url
+                contOverlayElm.innerHTML = `
+                <button type="button" id="close"><i class="fa-regular fa-circle-xmark"></i></button>
+                <img src="${url}" alt=""></img>
+                `
+            })
+        })
+}
+
+/**
+ * PRENDE LE IMMAGINI DALL'API E AGGIUNGE A DOM, RICHIAMA OVERLAY E REMOVE 
+ * @returns {any}
+ */
 function photoGen () {
     let result = "";
         axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) => {
@@ -26,25 +55,13 @@ function photoGen () {
                 `
             });
         rigaElm.innerHTML = result;
-        
-        const cardElm = document.querySelectorAll(".card");
-        cardElm.forEach((card, index) => {
-            card.addEventListener("click", function () {
-                overElm.classList.remove("hidden");
-                overElm.classList.add("flex");
-                const url = card.dataset.url
-                contOverlayElm.innerHTML = `
-                <button type="button" id="close"><i class="fa-regular fa-circle-xmark"></i></button>
-                <img src="${url}" alt=""></img>
-                `
-            })
+        overlay ();
         })
-
         closeElm.addEventListener("click", remove)
         overElm.addEventListener("click", remove)
-})   
 }
 
+// RICHIAMO FUNCTION
 photoGen ();
 
 
